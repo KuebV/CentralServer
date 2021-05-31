3dcd011b259f07c7bab90a07a54f7cfd9ced1030
KuebV <42746052+KuebV@users.noreply.github.com>
2021-05-31 12:10:23 -0700

    Files arent being added?

---
+++ CentralServerBot/Packet.cs
@@ -0,0 +1,24 @@
+﻿using Networking.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CentralServerBot
+{
+    public class Packet : EasyPacket
+    {
+        public override void handleDataFromServer(byte[] data)
+        {
+            base.handleDataFromServer(data);
+        }
+
+        public byte[] writePacket(string Action, string SteamID)
+        {
+            List<byte> data = new List<byte>();
+            writeString(Action, ref data);
+            writeString(SteamID, ref data);
+
+            return data.ToArray();
+        }
+    }
+}
---
+++ CentralServerBot/Program.cs
@@ -0,0 +1,56 @@
+﻿using Networking.Core;
+using System;
+using System.Threading;
+
+namespace CentralServerBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new DiscordBots.MainBot().RunBotAsync().GetAwaiter().GetResult();
+            Console.Read();
+        }
+
+        private static EasyClient client;
+        private static Packet packet = new Packet();
+
+        public static void SendPacketToServer(string Message)
+        {
+            string IP = "203.10.96.26";
+            int Port = 7500;
+
+            client = new EasyClient(IP, Port, packet);
+            client.ConnectToServer();
+            if (client.isConnected())
+                Log.Debug("Connected to Server");
+
+            else
+                Log.Error("Failed connection to Server");
+
+            string[] act = Message.Split('-');
+
+            client.SendPacketToServer(packet.writePacket(act[0], act[1]));
+
+            Thread.Sleep(3000);
+
+            client.Disconnect();
+        }
+
+    }
+
+    public class Log
+    {
+        public static void Info(string Text) =>
+            ColoredConsole.WriteLine(Text, ConsoleColor.Cyan);
+
+        public static void Error(string Text) =>
+            ColoredConsole.WriteLine(Text, ConsoleColor.Red);
+
+        public static void Debug(string Text) =>
+            ColoredConsole.WriteLine(Text, ConsoleColor.Green);
+    }
+
+
+
+}
